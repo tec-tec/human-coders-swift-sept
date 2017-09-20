df9544935cf75e826729433c4b0641da43e75f5f
Ludovic Ollagnier <ludovic@ollagnier.me>
2017-09-20 09:20:51 +0200

    Ajout du projet xcode de demo

---
+++ Projets/RestOoO/RestOoO/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  RestOoO
+//
+//  Created by Ludovic Ollagnier on 19/09/2017.
+//  Copyright © 2017 Ludovic Ollagnier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: - Properties
+
+    @IBOutlet weak var monLabel: UILabel!
+
+    //MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: - UI Actions
+
+    @IBAction func monAction(_ sender: Any) {
+
+    }
+
+    //MARK: - Public methods
+
+    //MARK: - Private methods
+
+}
+
